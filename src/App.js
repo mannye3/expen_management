5db0a853a840edc7eba481d764da275ac2c99206
manny3e <aboajahemmanuel@gmail.com>
2023-03-15 12:17:00 +0100

    view created and logic

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
-
-import Footer from './components/Footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
         <Router>
-
-        <Route exact path='/login' component={Login} />
+          <Route exact path='/login' component={Login} />
           <Route exact path='/' component={Home} /> 
-          <Footer/>
-         
-         
-          
-
-       
-        
-        
-
         </Router>
     </div>
     
